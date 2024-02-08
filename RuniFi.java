d4fc6fd400ba2f7c0656634e72a73f698e46afc0
raz cohen <raz.cohen03@post.runi.ac.il>
2024-02-08 15:26:02 +0200

    HW08 1st commit

--- RuniFi.java
+++ RuniFi.java
@@ -25,11 +25,11 @@ class RuniFi {
 
         // Displays the total duration
         System.out.println("\nTotal time duration of my list (in seconds):" + myList.totalDuration());
-
+        
         // Inserts a track to some location
         myList.add(1, track5);
         display("After adding Yesterday at location 1", myList);
-
+        
         // Tests the ability to find the locations of track titles
         System.out.println("\nIndex of Creep: " + myList.indexOf("Creep"));
         System.out.println("\nIndex of Shake It Off: " + myList.indexOf("Shake It Off"));
@@ -37,13 +37,13 @@ class RuniFi {
         // Removes some tracks
         myList.remove(2);
         display("After removing the track in location 2", myList);
-
+        
         myList.removeFirst();
         display("After removing the first track", myList);
-
+        
         myList.removeLast();
         display("After removing the last track", myList);
-
+        
         PlayList newList = new PlayList(10);
     
         // Creates some tracks and adds them to a new list
@@ -52,20 +52,21 @@ class RuniFi {
         newList.add(new Track("Shallow", "Lady Gaga", 217));
         newList.add(new Track("Woman", "Doja Cat", 172));
         display("New list", newList);
-
+        
         newList.remove("Yellow");
         display("New list after removing Yellow:", newList);
-
+        
         // Adds all the tracks from the new list to my list 
         myList.add(newList);
         display("My list after merging with new list", myList);
-
+        
         // Tests the ability to find the track with the shortest duration
         System.out.println("\nThe shortest track in my list is " + myList.titleOfShortestTrack());
-
+        
         // Sorts the list 
         myList.sortedInPlace();
         display("My list after sorting by increasing duration", myList);
+        /**/
     }
 
     private static void display(String str, PlayList list) {
